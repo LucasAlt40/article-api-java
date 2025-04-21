062d76e7f2a7a8ffdbcb1ddb362dc935896151c8
LucasAlt40 <lucas.alt40@gmail.com>
2025-04-21 19:46:18 -0300

    feat: adicionado configuração de segurança do spring security; feat: adicionado controller para categorias

---
+++ src/main/java/direto/grao/articleapi/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package direto.grao.articleapi.controller;
+
+import direto.grao.articleapi.model.Category;
+import direto.grao.articleapi.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getAllCategories());
+    }
+}
---
+++ src/main/java/direto/grao/articleapi/service/CategoryService.java
@@ -0,0 +1,20 @@
+package direto.grao.articleapi.service;
+
+import direto.grao.articleapi.model.Category;
+import direto.grao.articleapi.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
